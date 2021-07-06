f147665a917efb14d9cf0a5656632a19fe45e266
Gabriel Pires <gabriel.santos.pes@gmail.com>
2021-07-06 10:56:26 -0300

    Removendo knobs e migrando para essentials

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,9 +3,5 @@ module.exports = {
     '../src/pages/components/**/stories.mdx',
     '../src/pages/components/**/stories.@(js|jsx|ts|tsx)'
   ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-knobs'
-  ]
+  addons: ['@storybook/addon-essentials']
 }
